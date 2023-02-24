2666fcc07b58e7fc4402483766a8b9e545a22b90
Farkhodoff19 <farkhodoffcodes@gmail.com>
2023-02-24 15:15:07 +0300

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+const formCreate = document.getElementById("form-create");
+const formEdit = document.getElementById("form-edit");
+const listGroupTodo = document.getElementById("list-group-todo");
+
+const time = document.getElementById("time");
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
+
+/* time elements */
+const fullDay = document.getElementById("full-day");
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
+const closeEl = document.getElementById("close");
